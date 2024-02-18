434451af1ca615fe7e752be9c77051c0e09202c9
Cristina Patricia Aliaga Jimenez <caliaga-@c1r13s3.42madrid.com>
2024-02-18 21:35:08 +0100

    avance_dom18

--- docs/enviroment_manage.c
+++ docs/enviroment_manage.c
@@ -33,17 +33,60 @@ char *select_env(char **env)
     return (NULL);
 }
 
+size_t	ft_strlen(const char *str)
+{
+	size_t	a;
+
+	a = 0;
+	while (*(str + a) != '\0')
+		a++;
+	return (a);
+} 
+
+char	*ft_strjoin_slash(const char *s1, const char *s2)
+{
+	char	*sjoin;
+	size_t	size;
+	size_t	i;
+	size_t	j;
+
+	if (s1 == 0 || s2 == 0)
+		return (0);
+	size = ft_strlen(s1) + ft_strlen(s2) + 2; // +2 Añado slash
+	sjoin = (char *)malloc(size * sizeof(char));
+	if (sjoin == 0)
+		return (0);
+	i = -1;
+	while (++i < ft_strlen(s1))
+	{
+		sjoin[i] = s1[i];
+	}
+    sjoin[i] = '/'; // Le colocamos el slash se va a 26 líneas
+    i++;
+	j = -1;
+	while (++j < ft_strlen(s2))
+	{
+		sjoin[i + j] = s2[j];
+	}
+	sjoin[i + j] = '\0';
+	return (sjoin);
+}
+
 int main(int argc, char **argv, char **env)
 {
-    // ./a.out hola mundo sio
+    // ./a.out hola
     
     printf("Parámetros %i", argc);
     int a;
     a = -1;
     while (argv[++a])
-        printf("%s -->\n", argv[a]);
+        printf("%s --> ", argv[a]);
 
-    printf("%s", select_env(env));
+    char *ruta = select_env(env);
+    char *add = argv[1];
+    printf("1 -> %s | 2 -> %s \n", ruta, add);
+
+    printf("Join: %s\n", ft_strjoin_slash(ruta, argv[1]));
 
     return (0);
 }
