77e8f28ca90b75ea7d5400926fb4c00bd0f0a710
tonic_water <tonic_water@glass.com>
2024-04-22 19:32:13 +0200

    lun_22_abr_2024_17:32:13_UTC

--- docs/parse_quotes/swap_char.c
+++ docs/parse_quotes/swap_char.c
@@ -24,18 +24,41 @@ size_t	ft_strlen(const char *str)
 	return (a);
 }
 
-void find_quote(int pos[], int *p, char i, int *quotes)
-{
-    quotes += 1;
-    pos[p] = i;
-    p += 1;
-}
-
 // CONTAR LAS COMILLAS DE UNA CADENA. type 39 o 34
 int count_quotes(char *str, int type)
 {
     int i;
     int quotes;
+
+    quotes = 0;
+    i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] == type && i == 0) // La comilla de apertura es LA PRIMERA.
+            quotes += 1;
+        // La comilla de apertura tiene un ESPACIO ANTES. Puede ser comilla de cierre.
+        // He tenido problemas de conteo con la expresión: str[i] == 39 && str[i - 1] == 32
+        if (str[i] == 32 && str[i + 1] == type)
+            quotes += 1;
+        if (str[i] == type && str[i + 1] == 32) // La comilla de cierre tiene un ESPACIO DESPUES.
+            quotes += 1;
+        if (str[i] == type && i == (int)ft_strlen(str)-1) // La comilla de cierre es LA ULTIMA.
+            quotes += 1;
+        i++;
+    }
+   return (quotes); 
+}
+
+void find_quote(int *pos, int *p, char i)
+{
+    pos[*p] = i;
+    *p += 1;
+}
+// ARRAY CON LAS POSICIONES DE LAS COMILLAS
+int *quote_position(char *str, int type)
+{
+    int i;
+    int *quotes;
     int pos[32];
     int p;
 
@@ -44,26 +67,19 @@ int count_quotes(char *str, int type)
     i = 0;
     while (str[i] != '\0')
     {
-        if (str[i] == type && i == 0) // La comilla de apertura es LA PRIMERA.
-            find_quote(pos, &p, i, &quotes);
-        // La comilla de apertura tiene un ESPACIO ANTES. Puede ser comilla de cierre.
-        // He tenido problemas de conteo con la expresión: str[i] == 39 && str[i - 1] == 32
+        if (str[i] == type && i == 0)
+            find_quote(pos, &p, i);
         if (str[i] == 32 && str[i + 1] == type)
-            find_quote(pos, &p, i, &quotes);
-        if (str[i] == type && str[i + 1] == 32) // La comilla de cierre tiene un ESPACIO DESPUES.
-            find_quote(pos, &p, i, &quotes);
-        if (str[i] == type && i == (int)ft_strlen(str)-1) // La comilla de cierre es LA ULTIMA.
-            find_quote(pos, &p, i, &quotes);
+            find_quote(pos, &p, i);
+        if (str[i] == type && str[i + 1] == 32)
+            find_quote(pos, &p, i);
+        if (str[i] == type && i == (int)ft_strlen(str)-1)
+            find_quote(pos, &p, i);
         i++;
     }
-   return (quotes); 
+    *quotes = *pos;
+    return (quotes);
 }
-
-//int *quote_position()
-//{
-//    return (quotes);
-//}
-
 // Las comillas simpre han de tener una apertura y un cierre. Por lo que van por pares. 
 // El resto de dividir el número total de comillas por 2 será 0.
 
@@ -103,6 +119,9 @@ int main()
 
     char str0[] = "Hola 'lolo'";
     printf("%i\n", count_quotes(str0, 39));
+    
+    printf("%i\n", quote_position(str0, 39)[0]);
+    printf("%i\n", quote_position(str0, 39)[1]);
 
     char str1[] = "Hola mundo 'hi lolo'";
     quotes_swap_char(str1, ' ', '_');
