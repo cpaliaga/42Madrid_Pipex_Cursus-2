04f32915445899a516e764bb491bdb8344840aff
tonic_water <tonic_water@glass.com>
2024-04-22 16:26:42 +0200

    lun_22_abr_2024_14:26:42_UTC

---
+++ docs/parse_quotes/swap_char.c
@@ -0,0 +1,121 @@
+#include <stdio.h>
+
+// Esta función localiza un carácter en un string y lo reemplaza por otro. 
+// Modifica el string original y Retorna void en caso de cadena nula. 
+void swap_char(char *str, int *i, char old, char new)
+{
+    if (str == NULL)
+        return;
+    while (str[*i] != '\0')
+    {
+        if (str[*i] == old)
+            str[*i] = new;
+        *i += 1;
+    }
+}
+
+size_t	ft_strlen(const char *str)
+{
+	size_t	a;
+
+	a = 0;
+	while (*(str + a) != '\0')
+		a++;
+	return (a);
+}
+
+// CONTAR LAS COMILLAS DE UNA CADENA. type 39 o 34
+int count_quotes(char *str, int type)
+{
+    int i;
+    int quotes;
+    int len;
+
+    len = ft_strlen(str);
+    quotes = 0;
+    i = 0;
+    while (str[i] != '\0')
+    {
+        if (str[i] == type && i == 0) // La comilla de apertura es LA PRIMERA.
+            quotes++;
+        // La comilla de apertura tiene un ESPACIO ANTES. Puede ser comilla de cierre.
+        // He tenido problemas de conteo con la expresión: str[i] == 39 && str[i - 1] == 32
+        if (str[i] == 32 && str[i + 1] == type)
+            quotes++;
+        if (str[i] == type && str[i + 1] == 32) // La comilla de cierre tiene un ESPACIO DESPUES.
+            quotes++;
+        if (str[i] == type && i == len-1) // La comilla de cierre es LA ULTIMA.
+            quotes++;
+        i++;
+    }
+   return (quotes); 
+}
+
+int *quote_position()
+{
+    int i;
+    int quotes[2];
+    int len;
+
+    quotes[0] = count_quotes(str, 39);
+    quotes[1] = count_quotes(str, 34);
+    len = ft_strlen(str);
+
+    if (quotes[0]%2 != 0)
+    {
+        printf("Las comillas están abiertas\n");
+    }
+        printf("Las comillas están cerradas\n");
+    printf("Len: %d, old %c new %c \n", len, old, new);
+    printf("\nQuotes: simples %d - doubles %d \n", quotes[0], quotes[1]);
+    return (quotes);
+}
+
+// Las comillas simpre han de tener una apertura y un cierre. Por lo que van por pares. 
+// El resto de dividir el número total de comillas por 2 será 0.
+
+void quotes_swap_char(char *str, char old, char new)
+{
+    //int i;
+    int quotes[2];
+    int len;
+
+    quotes[0] = count_quotes(str, 39);
+    quotes[1] = count_quotes(str, 34);
+    len = ft_strlen(str);
+
+    //while (str[i] != '\0')
+    //{
+        //if (str[i] == old)
+            //str[i] = new;
+        //i++;
+    //}
+
+
+    if (quotes[0]%2 != 0)
+    {
+        printf("Las comillas están abiertas\n");
+    }
+        printf("Las comillas están cerradas\n");
+    printf("Len: %d, old %c new %c \n", len, old, new);
+    printf("\nQuotes: simples %d - doubles %d \n", quotes[0], quotes[1]);
+}
+
+int main()
+{
+    int i = 0;
+    char str[] = "Hola mundo 'hi lolo'";
+    swap_char(str, &i, ' ', '_');
+    printf("%s\n", str);
+
+    char str0[] = "Hola 'lolo'";
+    printf("%i\n", count_quotes(str0, 39));
+
+    char str1[] = "Hola mundo 'hi lolo'";
+    quotes_swap_char(str1, ' ', '_');
+    printf("%s\n", str1);
+
+    return (0);
+}
+
+// gcc -Wall -Wextra -Werror swap_char.c -o swap_char && ./swap_char
